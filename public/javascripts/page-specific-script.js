2d1869358b664066918c7a7d28247212488b4a21
molleman <molleman@gmail.com>
2014-03-08 11:42:26 +0000

    if is nan

--- public/javascripts/page-specific-script.js
+++ public/javascripts/page-specific-script.js
@@ -1,8 +1,10 @@
 var content_area = $("#content_area");
 function load_content() {
 	$.get("/getdata", function(data) {
-		content_area.html('0'+data);
-		load_content();
+		if(!isNaN(data)){
+			content_area.html('0'+data);
+			load_content();
+		}
 	});
 }
 load_content();
