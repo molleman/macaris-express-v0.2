3488060858b4dd50504be745f49ffc0396602663
molleman <molleman@gmail.com>
2014-03-09 22:08:51 +0000

    Add interval to fetch Facebook likes
    We now store the number of likes in the like variable

--- facebook.js
+++ facebook.js
@@ -1,9 +1,26 @@
 var graph = require('fbgraph');
 
+var likes;
+
+
 
 exports.getLikes = function (response) {
+	if(likes === undefined){
+		response.json("10000");
+		console.log('likes is undefined')
+		return likes;
+	}else{
+		console.log('likes is '+likes['likes']);
+		response.json(likes['likes']);
+		return likes;
+	}
+}
+
+function fetchLikes(){
 	graph.get("153299718040481", function(err, res) {
-		response.json(res['likes']);
+		likes = res['likes'];
 	    return res;
 	});
 }
+
+setInterval(fetchLikes,1000); 
